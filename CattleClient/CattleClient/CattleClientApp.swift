0615a222866a39749b95f75a456a881af99b2bf9
Elie (Jeon Jimin) <cjjunn0827@gmail.com>
2023-04-07 17:53:54 +0900

    [Chore] 프로젝트 파일 추가

---
+++ CattleClient/CattleClient/CattleClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  CattleClientApp.swift
+//  CattleClient
+//
+//  Created by 전지민 on 2023/04/07.
+//
+
+import SwiftUI
+
+@main
+struct CattleClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CattleDashboard/CattleDashboard/CattleDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CattleDashboardApp.swift
+//  CattleDashboard
+//
+//  Created by 전지민 on 2023/04/07.
+//
+
+import SwiftUI
+
+@main
+struct CattleDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
